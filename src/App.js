55dc16833c1e1512cecdf25d52b9c70d081c8f53
Brent Ross Habawel <rosshabawel12@gmail.com>
2023-11-29 21:27:10 +0800

    added all functions needed

--- src/App.js
+++ src/App.js
@@ -17,24 +17,44 @@ export default function App() {
       console.log(data);
       setStoreApi(data.rates[toCurr]);
     }
+    if (fromCurr === toCurr) {
+      setStoreApi(amount);
+    }
     fetchCurrency();
-  }, []);
+  }, [amount, fromCurr, toCurr]);
   return (
     <div>
-      <input type="text" />
-      <select>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <select>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <p>OUTPUT</p>
+      <Input amount={amount} setAmount={setAmount} />
+      <Options onCurr={fromCurr} setOnCurr={setFromCurr} />
+      <span> to </span>
+      <Options onCurr={toCurr} setOnCurr={setToCurr} />
+      <Output onValue={storeApi} />
     </div>
   );
+}
+
+function Input({ amount, setAmount }) {
+  return (
+    <input
+      type="text"
+      placeholder="Enter a value..."
+      amount={amount}
+      onChange={(e) => setAmount(e.target.value)}
+    />
+  );
+}
+
+function Options({ onCurr, setOnCurr }) {
+  return (
+    <select value={onCurr} onChange={(e) => setOnCurr(e.target.value)}>
+      <option value="USD">USD</option>
+      <option value="EUR">EUR</option>
+      <option value="CAD">CAD</option>
+      <option value="INR">INR</option>
+    </select>
+  );
+}
+
+function Output({ onValue }) {
+  return <p>{onValue}</p>;
 }
