48b85855cffdf87247ebbd151d5a95bff434abf3
Brent Ross Habawel <rosshabawel12@gmail.com>
2023-11-29 20:19:14 +0800

    added state and effect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
+import { useEffect, useState } from "react";
+
 export default function App() {
+  return <Converter />;
+}
+
+function Converter() {
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("EUR");
+
+  useEffect(() => {
+    async function fetchCurrency() {
+      try {
+        let res = await fetch(
+          `https://api.frankfurter.app/latest?amount=100&from=${fromCurrency}&to=${toCurrency}`
+        );
+        if (!res.ok) throw new Error(`Failed to fetch`);
+
+        let data = await res.json();
+        console.log(data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+    fetchCurrency();
+  }, [fromCurrency, toCurrency]);
+
+  function handleFromCurrency(currency) {
+    setFromCurrency(currency);
+  }
+
+  function handleToCurrency(currency) {
+    setToCurrency(currency);
+  }
+
   return (
     <div>
       <input type="text" />
-      <select>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <select>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <p>OUTPUT</p>
+      <Options onChange={handleFromCurrency} />
+      <span> to </span>
+      <Options onChange={handleToCurrency} value={"EUR"} />
+      <Output />
     </div>
   );
+}
+
+function Options({ onChange, value }) {
+  function handleCurrency(e) {
+    const currency = e.target.value;
+    onChange(currency);
+  }
+  return (
+    <select onChange={handleCurrency} value={value}>
+      <option value="USD">USD</option>
+      <option value="EUR">EUR</option>
+      <option value="CAD">CAD</option>
+      <option value="INR">INR</option>
+    </select>
+  );
+}
+
+function Output() {
+  return <p>OUTPUT</p>;
 }
