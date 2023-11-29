638883bb79d032f9499d46a7f2feac8f4cbbd669
Brent Ross Habawel <rosshabawel12@gmail.com>
2023-11-29 21:12:12 +0800

    added useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
+import { useEffect } from "react";
+
 export default function App() {
+  useEffect(() => {
+    async function fetchCurrency() {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+      );
+      const data = await res.json();
+      console.log(data);
+    }
+    fetchCurrency();
+  }, []);
   return (
     <div>
       <input type="text" />
